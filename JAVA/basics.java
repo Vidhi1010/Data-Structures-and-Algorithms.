2e9b1a7a2df227626a363a3a1be3e9754480ddf5
Vidhi1010 <vidhisinghvp1010@gmail.com>
2024-08-14 00:34:22 +0530

    new

--- JAVA/basics.java
+++ JAVA/basics.java
@@ -1,11 +1,12 @@
+import java.util.Scanner;
 
 public class basics {
     public static void main(String[] args) {
-        int num1 = 3;
+        /* int num1 = 3;
         int num2 = 4;
         int sum = num1 + num2;
         System.out.println(sum);
-        System.out.println("Hello World");
+        System.out.println("Hello World"); */
     
 
 
@@ -19,7 +20,44 @@ public class basics {
     double d = 5.6;
     float f = 5.6f;
     char  c = 'v';
-    boolean b = true;
- 
+    boolean bo = true;
+
+    //Non-primitive   - String, Array, Class, Objects, Interface
+
+    //Sum of a and b
+
+    Scanner sc = new Scanner(System.in);
+
+    /* int a = sc.nextInt();
+    int b = sc.nextInt();
+    int sum = a + b;
+    System.out.println(sum);*/
+
+    /* String name = sc.nextLine();
+    System.out.println(name);*/
+
+    /* float price = sc.nextFloat();
+    System.out.println(price); */
+
+    /* double amount = sc.nextDouble();
+    System.out.println(amount); */
+    //System.out.printf("%.0f%n", amount);
+
+
+    //area of a circle 
+
+   /*  float r = sc.nextFloat();
+    float area = (3.14f * r * r);
+    System.out.println(area); */
+
+    //TYPE PROMOTION
+    char a = 'a';
+    char b = 'b';
+    System.out.println((int)(a));
+    System.out.println((int)(b));
+    System.out.println(b - a);
+
+
+
     }
 }
