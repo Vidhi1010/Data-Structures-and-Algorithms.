9144bcbc8930b5463f32dfef0b6502f8d9e114d6
Vidhi1010 <vidhisinghvp1010@gmail.com>
2024-08-14 23:36:36 +0530

    basics

--- JAVA/basics.java
+++ JAVA/basics.java
@@ -2,62 +2,165 @@ import java.util.Scanner;
 
 public class basics {
     public static void main(String[] args) {
-        /* int num1 = 3;
-        int num2 = 4;
-        int sum = num1 + num2;
-        System.out.println(sum);
-        System.out.println("Hello World"); */
-    
+        /*
+         * int num1 = 3;
+         * int num2 = 4;
+         * int sum = num1 + num2;
+         * System.out.println(sum);
+         * System.out.println("Hello World");
+         */
 
+        // Data types
+        // primitive - predefind- integer, float, character, boolean
 
-    //Data types
-    //primitive - predefind- integer, float, character, boolean
-    
-    int i = 10;
-    byte by = 127;
-    short sh = 558;
-    long l = 5885l;
-    double d = 5.6;
-    float f = 5.6f;
-    char  c = 'v';
-    boolean bo = true;
+        int i = 10;
+        byte by = 127;
+        short sh = 558;
+        long l = 5885l;
+        double d = 5.6;
+        float f = 5.6f;
+        char c = 'v';
+        boolean bo = true;
 
-    //Non-primitive   - String, Array, Class, Objects, Interface
+        // Non-primitive - String, Array, Class, Objects, Interface
 
-    //Sum of a and b
+        // Sum of a and b
 
-    Scanner sc = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
 
-    /* int a = sc.nextInt();
-    int b = sc.nextInt();
-    int sum = a + b;
-    System.out.println(sum);*/
+        /*
+         * int a = sc.nextInt();
+         * int b = sc.nextInt();
+         * int sum = a + b;
+         * System.out.println(sum);
+         */
 
-    /* String name = sc.nextLine();
-    System.out.println(name);*/
+        /*
+         * String name = sc.nextLine();
+         * System.out.println(name);
+         */
 
-    /* float price = sc.nextFloat();
-    System.out.println(price); */
+        /*
+         * float price = sc.nextFloat();
+         * System.out.println(price);
+         */
 
-    /* double amount = sc.nextDouble();
-    System.out.println(amount); */
-    //System.out.printf("%.0f%n", amount);
+        /*
+         * double amount = sc.nextDouble();
+         * System.out.println(amount);
+         */
+        // System.out.printf("%.0f%n", amount);
 
+        // area of a circle
 
-    //area of a circle 
+        /*
+         * float r = sc.nextFloat();
+         * float area = (3.14f * r * r);
+         * System.out.println(area);
+         */
 
-   /*  float r = sc.nextFloat();
-    float area = (3.14f * r * r);
-    System.out.println(area); */
+        // TYPE PROMOTION
+        /*
+         * char a = 'a';
+         * char b = 'b';
+         * System.out.println((int)(a));
+         * System.out.println((int)(b));
+         * System.out.println(b - a);
+         */
 
-    //TYPE PROMOTION
-    char a = 'a';
-    char b = 'b';
-    System.out.println((int)(a));
-    System.out.println((int)(b));
-    System.out.println(b - a);
+        // switch case
+        /*
+         * System.out.println("Enter a:");
+         * int a = sc.nextInt();
+         * System.out.println("Enter b:");
+         * int b = sc.nextInt();
+         * System.out.println("Enter operator:");
+         * char operator = sc.next().charAt(0);
+         * 
+         * switch (operator) {
+         * case '+':
+         * System.out.println(a + b);
+         * break;
+         * case '-':
+         * System.out.println(a - b);
+         * break;
+         * case '*':
+         * System.out.println(a * b);
+         * break;
+         * case '/':
+         * System.out.println(a / b);
+         * break;
+         * case '%':
+         * System.out.println(a % b);
+         * break;
+         * 
+         * default:
+         * System.out.println("wrong operator");
+         * break;
+         * }
+         */
 
+        /*
+         * System.out.println("Enter a:");
+         * int a = sc.nextInt();
+         * 
+         * if (a > 0) {
+         * System.out.println(a + " is a positive number");
+         * }else {
+         * System.out.println(a + " is a negative number");
+         * }
+         */
 
+        /*
+         * System.out.println("Enter temp:");
+         * int temp = sc.nextInt();
+         * 
+         * if (temp > 100) {
+         * System.out.println("You have fever");
+         * }else {
+         * System.out.println("You don't have fever");
+         * }
+         */
 
+        /* System.out.println("Enter num:");
+        int num = sc.nextInt();
+
+        switch (num) {
+            case 1:
+                System.out.println("Monday");
+                break;
+            case 2:
+                System.out.println("Tuesday");
+                break;
+            case 3:
+                System.out.println("Wednesday");
+                break;
+            case 4:
+                System.out.println("Thursday");
+                break;
+            case 5:
+                System.out.println("Friday");
+                break;
+            case 6:
+                System.out.println("Saturday");
+                break;
+            case 7:
+                System.out.println("Sunday");
+                break;
+
+            default:
+                System.out.println("wrong day");
+                break;
+        } */
+
+        /* System.out.println("Enter year:");
+        int year = sc.nextInt();
+
+        if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
+            System.out.println(year + " is a leap year");
+        }else {
+            System.out.println(year + " is a normal year");
+        } */
+        
     }
 }
---
+++ Loops.java
@@ -0,0 +1,139 @@
+import java.util.*;
+
+public class Loops {
+
+    // WHILE LOOP
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        /* int counter = 0;
+        while (counter < 100) {
+            System.out.println("Hello World");
+            counter++;
+        } */
+
+        /* int num = 1;
+        while (num <= 10) {
+            System.out.println(num);
+            num++;
+        } */
+
+        //1 to n
+        /* 
+        int counter = 1;
+        System.out.println("Enter a number");
+        int num = sc.nextInt();
+        while (counter <= num) {
+            System.out.println(counter);
+            counter++;
+        } */
+
+        //sum of first n natural numbers
+        
+        /* System.out.println("Enter a number: ");
+        int n = sc.nextInt();
+        int i = 1;
+        int sum = 0;
+        while (i <= n) {
+            sum = sum + i;
+            //sum += i;
+            i++;
+        } 
+        System.out.println("Sum is: " + sum); */
+
+        //FOR LOOP
+        
+        /* int n = sc.nextInt();
+        for(int i = 0; i <= n; i++) {
+            System.out.println("Hello World");
+        } */
+
+        //Print square pattern
+        /* for(int line = 1; line <= 4; line++) {
+            System.out.println("****");
+        }*/
+
+        //Print reverse of a number
+         
+       /* int n = 10899;
+          while (n > 0) {
+            int lastDigit = n % 10;
+            System.out.print(lastDigit + " ");
+            n = n / 10;
+        }
+        System.out.println(); */
+        /* int n = 10899;
+        int rev = 0;
+        while (n > 0) {
+            int lastDigit = n % 10;
+            rev = (rev * 10) + lastDigit;
+            n = n/10;
+        }
+        System.out.println(rev); */
+
+        //Do While Loop
+
+        /* int counter = 1;
+        do{
+            System.out.println("Hello World");
+            counter++;
+        }while(counter <= 10); */
+
+        //keep entering numbers till user enters a multiple of 10
+       /*  do{
+            System.out.println("Enter your number: ");
+            int n = sc.nextInt();
+            if(n % 10 == 0) {
+                break;
+            }
+            System.out.println(n);
+        } while(true);
+        */
+        //Continue 
+        /* for(int i = 1; i <=5; i++) {
+            if(i == 3) {
+                continue;
+            }
+            System.out.println(i);
+        } */
+
+        //
+        /* do{
+            System.out.println("Enter your number: ");
+            int n = sc.nextInt();
+            if(n % 10 == 0) {
+                continue;
+            }
+            System.out.println("number was : " + n);
+        } while(true); */
+
+        //Prime Number
+        int num = sc.nextInt();
+        /* int i = 2;
+        while (i < num) {
+            if (num % i == 0) {// n is a multiple of i
+                System.out.println("Not prime");
+            }
+            i++;
+        }
+        System.out.println("prime"); */
+
+        if (num == 2) {
+            System.out.println("n is prime");
+        }
+        boolean isPrime = true;
+        for(int i = 2; i <= Math.sqrt(num); i++) {
+            if (num % i == 0) {
+                isPrime = false;
+            }
+        }
+        if (isPrime == true) {
+            System.out.println("n is prime");
+        }else{
+            System.out.println("n is not prime");
+        }
+        
+    }
+
+}
