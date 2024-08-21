5d6ae95b8ba11a0b20766e184b799d694067cacf
Vidhi1010 <vidhisinghvp1010@gmail.com>
2024-08-21 21:01:39 +0530

    new

--- JAVA/Function.java
+++ JAVA/Function.java
@@ -33,15 +33,46 @@ public class Function {
         return f;
 
     }
+    public static int binCoeff(int n, int r) {
+        int fact_n = factorial(n);
+        int fact_r = factorial(r);
+        int fact_nmr = factorial(n - r);
+
+        int binCoeff = fact_n/ (fact_r * fact_nmr);
+        return binCoeff;
+
+    }
+
+    //func to cal sum of 2 numbers
+    public static int sum(int a, int b) {
+        return a + b;
+    }
+
+    //func to cal sum of 3 numbers
+    public static int sum(int a, int b, int c) {
+        return a + b + c;
+    }
+
+    //func to cal float sum
+    public static float sum(float a, float b) {
+        return a + b;
+    }
+
+    
     public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-        int b = sc.nextInt();
+        //int a = sc.nextInt();
+        //int b = sc.nextInt();
          //printHelloWorld();
         //calculateSum(a, b);
         //swap(a, b);
         //calProduct(a, b);
         //factorial(5);
         //System.out.println(factorial(5));
+        //binCoeff(5, 2);
+        // System.out.println(binCoeff(5, 2));
+        System.out.println(sum(3, 5));
+        System.out.println(sum(3, 5, 2));
+        System.out.println(sum(3.5f, 5.5f));
     }
 }
