8b2b169fef3f328ea41b5fc31f3d23b091561210
Vidhi1010 <vidhisinghvp1010@gmail.com>
2024-08-21 23:29:32 +0530

    new

--- JAVA/Function.java
+++ JAVA/Function.java
@@ -58,7 +58,83 @@ public class Function {
         return a + b;
     }
 
+    //prime number
+
+    /* public static boolean isPrime(int n) {
+        boolean isPrime = true;
+        for( int i = 2; i <= n-1; i++) {
+            if (n % i == 0) {
+                isPrime = false;
+                break;
+            }
+        }
+        return isPrime;
+    } */
+
+   /*  public static boolean isPrime(int n) {
+        //corner cases
+        if (n == 2) {
+            return true;
+        }
+        for( int i = 2; i <= n-1; i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    } */
     
+    public static boolean isPrime(int n) {
+        if (n == 2) {
+            return true;
+        }
+        for(int i = 2; i <= Math.sqrt(n); i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+    
+    //print all primes in a range
+
+    public static void primeInRange(int n) {
+        for(int i = 2; i <= n; i++) {
+            if (isPrime(i)) {//true
+                System.out.print(i + " ");
+            }
+        }
+        System.out.println();
+    }
+
+    public static void binToDec(int binNum) {
+        int myNum = binNum;
+        int pow = 0;
+        int decNum = 0;
+
+        while (binNum > 0) {
+            int lastDigit = binNum % 10 ;
+            decNum = decNum + (lastDigit * (int)Math.pow(2, pow));
+            pow++;
+            binNum = binNum/10;
+        }
+        System.out.println("decimal of " + myNum + " = " + decNum);
+    }
+
+    public static void decToBin(int n) {
+        int myNum = n;
+        int pow = 0;
+        int binNum = 0;
+        while (n > 0) {
+            int rem = n % 2 ;
+            binNum = binNum + (rem * (int)Math.pow(10, pow));
+
+            pow++;
+            n = n/2;
+        }
+        System.out.println("binary of " + myNum + " = " + binNum);
+    }
+
     public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
         //int a = sc.nextInt();
@@ -71,8 +147,13 @@ public class Function {
         //System.out.println(factorial(5));
         //binCoeff(5, 2);
         // System.out.println(binCoeff(5, 2));
-        System.out.println(sum(3, 5));
+        /* System.out.println(sum(3, 5));
         System.out.println(sum(3, 5, 2));
-        System.out.println(sum(3.5f, 5.5f));
+        System.out.println(sum(3.5f, 5.5f)); */
+        //System.out.println(isPrime(16));
+        //primeInRange(20);
+        //binToDec(1010);
+        decToBin(7);
+
     }
 }
