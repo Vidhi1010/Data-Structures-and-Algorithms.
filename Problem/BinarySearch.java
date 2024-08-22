31a8a2c2f18688189a9be617c8e31663f51dce77
Vidhi1010 <vidhisinghvp1010@gmail.com>
2024-08-23 01:50:48 +0530

    Binary Search

---
+++ Problem/BinarySearch.java
@@ -0,0 +1,26 @@
+package Problem;
+
+public class BinarySearch {//prerequisite - sorted arrays
+
+    public static int binarySearch(int[] numbers, int key) {
+        int start = 0, end = numbers.length;
+
+        while (start <= end) {
+            int mid = (start + end)/2;
+
+            if (numbers[mid] == key) {
+                return mid;
+            } else if (numbers[mid] < key) {//right
+                start = mid + 1;
+            } else {//left
+                end = mid - 1;
+            }
+        }
+        return -1;
+    }
+    public static void main(String[] args) {
+        int[] sortedArray = {2, 4, 7, 10, 13, 18, 20};
+        int key = 13;
+        System.out.println("index for key is : " + binarySearch(sortedArray, key));
+    }
+}
