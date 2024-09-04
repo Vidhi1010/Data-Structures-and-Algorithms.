0fd1664a89293adc4f45cad4763a8580641f73b2
Vidhi1010 <vidhisinghvp1010@gmail.com>
2024-09-05 00:09:21 +0530

    new

--- Arrays/SmallestNumber.java
+++ Arrays/SmallestNumber.java
@@ -1,14 +1,11 @@
-package Arrays;
+import java.util.Scanner;
+//Package Arrays;
+/*
+Find the smallest number in an array of integers.
+
+*/
 
 public class SmallestNumber {
-    public static void main(String args[]) {
-
-        int arr1[] = { 2, 5, 1, 3, 0 };
-        System.out.println("The smallest element in the array is: " + SmallestElement(arr1));
-
-        int arr2[] = { 8, 10, 5, 7, 9 };
-        System.out.println("The smallest element in the array is: " + SmallestElement(arr2));
-    }
 
     static int SmallestElement(int arr[]) {
         int min = arr[0];
@@ -19,4 +16,26 @@ public class SmallestNumber {
         }
         return min;
     }
+    public static void main(String args[]) {
+
+        /* int arr1[] = { 2, 5, 1, 3, 0 };
+        System.out.println("The smallest element in the array is: " + SmallestElement(arr1));
+
+        int arr2[] = { 8, 10, 5, 7, 9 };
+        System.out.println("The smallest element in the array is: " + SmallestElement(arr2)); */
+
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+
+        for(int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        
+        int smallest = SmallestElement(arr);
+        System.out.println("The smallest element in the array is: " + smallest);
+        sc.close();
+    }
+
 }
