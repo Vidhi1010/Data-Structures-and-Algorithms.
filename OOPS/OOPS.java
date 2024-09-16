1be5783e55a3082ef07d6c7c2053f5602e95abba
Vidhi1010 <vidhisinghvp1010@gmail.com>
2024-09-16 12:05:01 +0530

    new

--- OOPS/OOPS.java
+++ OOPS/OOPS.java
@@ -2,39 +2,53 @@ package OOPS;
 
 public class OOPS {
     public static void main(String[] args) {
-        Pen p1 = new Pen();//constructor.....created a pen object called p1
-        p1.setColor("Blue");
-        System.out.println(p1.color);
-        p1.setTip(5);
-        System.out.println(p1.tip);
-        p1.color = "Red";
-        System.out.println(p1.color);
 
-        BankAccount myAcc = new BankAccount();
+        Pen p1 = new Pen();//constructor.....created a pen object called p1
+
+        p1.setColor("Blue");
+        System.out.println(p1.getColor());
+
+        p1.setTip(5);
+        System.out.println(p1.getTip());
+
+        p1.setColor("Red");
+        System.out.println(p1.getColor());
+
+        /* BankAccount myAcc = new BankAccount();
         myAcc.username = "Vidhi";
-        myAcc.setPassward("123@Vidhi");
+        myAcc.setPassward("123@Vidhi"); */
 
     }
     
 }
 
-class BankAccount {
+/* class BankAccount {
     public String username;
     private String password;
 
     public void setPassward(String pwd) {
         password = pwd;
     }
-}
+} */
 class Pen {
-    String color;
-    int tip;
+    private String color;// Instance variable
+    private int tip;// Instance variable
 
+    //getter
+    String getColor() {
+        return this.color;
+    }
+
+    int getTip() {
+        return this.tip;
+    }
+
+    //setter
     void setColor(String newColor) {
-        color = newColor;
+        this.color = newColor;// 'this.color' refers to the instance variable, 'newColor' is the method parameter
     }
 
     void setTip(int  newTip) {
-        tip = newTip;
+        this.tip = newTip;// 'this.tip' refers to the instance variable, 'newTip' is the method parameter
     }
 }
